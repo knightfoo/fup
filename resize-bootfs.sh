26d99a8f07eb7017d3fce22d0dd34dba720729d6
Charlie Root <root@cache-1.gts.nnv.pl>
2017-08-08 16:24:36 +0200

    freebsd-swap

--- resize-bootfs.sh
+++ resize-bootfs.sh
@@ -1,3 +1,29 @@
-#!/bin/sh -x
+#!/bin/sh
 
+
+freebsd-boot() {
+	gpart show -p | awk '/freebsd-boot/{ print $3 };' | while read dysk;
+	do
+		echo "Dysk $dysk"
+		d_=$(echo $dysk|sed 's/\(.*\)p./\1/')
+		i_=$(echo $dysk|sed 's/.*\(.\)$/\1/')
+		echo "$d_ - $i_"
+	done
+}	
+
+freebsd-swap() {
+	gpart show -p | awk '/freebsd-boot/{ print $3 };' | while read dysk;
+	do
+		echo "Dysk $dysk"
+		d_=$(echo $dysk|sed 's/\(.*\)p./\1/')
+		b_i=$(echo $dysk|sed 's/.*\(.\)$/\1/')
+		gpart show -p $d_ | awk '/freebsd-swap/{ print $3 };' | while read s_dysk;
+		do	
+			s_i=$(echo $s_dysk|sed 's/.*\(.\)$/\1/')
+			echo "Dysk: $d_ --- bootfs: $b_i --- swap: $s_i"
+		done				
+	done
+}	
+
+freebsd-swap
 
