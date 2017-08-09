2de1d6afd30b54accb025ca412c85e93ff13ff90
Charlie Root <root@cache-1.gts.nnv.pl>
2017-08-09 11:04:32 +0200

    resize-bootfs.sh - swap off

--- resize-bootfs.sh
+++ resize-bootfs.sh
@@ -14,16 +14,30 @@ freebsd-boot() {
 freebsd-swap() {
 	gpart show -p | awk '/freebsd-boot/{ print $3 };' | while read dysk;
 	do
-		echo "Dysk $dysk"
 		d_=$(echo $dysk|sed 's/\(.*\)p./\1/')
 		b_i=$(echo $dysk|sed 's/.*\(.\)$/\1/')
 		gpart show -p $d_ | awk '/freebsd-swap/{ print $3 };' | while read s_dysk;
 		do	
 			s_i=$(echo $s_dysk|sed 's/.*\(.\)$/\1/')
-			echo "Dysk: $d_ --- bootfs: $b_i --- swap: $s_i"
+			if [ $s_i -eq 2 ];
+			then
+				echo "Dysk: $d_ --- bootfs: $b_i --- swap: $s_i"
+			fi				
 		done				
 	done
 }	
 
-freebsd-swap
+swap() {
 
+	swapctl -l | grep dev | awk '{print $1}'| while read swap_dev;
+	do
+		if echo $swap_dev | grep '/mirror/' 1> /dev/null 2> /dev/null;
+		then
+			echo mirror
+		fi
+		
+
+	done
+}
+
+swap
